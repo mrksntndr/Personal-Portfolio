9a37d1bd54f41518f93e0e1d1f8b93e46e4888ff
mrksntndr <mrksntndr@gmail.com>
2024-09-18 02:57:59 +0800

    Intro, about me, and work experience done

---
+++ my-project/src/components/About.jsx
@@ -0,0 +1,25 @@
+import aboutImg from "../assets/dog.jpg";
+import { ABOUT_TEXT } from "../constants";
+export const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h2 className="my-20 text-center text-4xl">
+            About
+            <span className="text-neatural-500"> Me</span>
+        </h2>
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 flex justify-center lg:justify-start lg:pl-8">
+                    <img className="rounded-2xl w-96 h-auto" src={aboutImg} alt="my dog"/>
+             </div>
+             <div className="w-full lg:w-1/2">
+                <div className="flex flex-col items-center lg:items-start">
+                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                    {ABOUT_TEXT}
+                    </p>
+                </div>
+            </div>
+        </div> 
+    </div>
+  );
+};
+export default About;
--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,11 +1,33 @@
+import { INTRO } from "../constants";
+import profilePic from "../assets/Grad.png";
+
 export const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
-            <div className="flex flex-col items-center lg:items-start">
-                <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Mark Adrian Santander</h1>
-            </div>
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Mark Adrian Santander
+            </h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 
+              to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                Software Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {INTRO}
+            </p>
+          </div>
         </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center lg:justify-end">
+            <img src={profilePic} alt="Grad" className="w-96 h-auto lg:w-80"/>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+
+export default Hero;
